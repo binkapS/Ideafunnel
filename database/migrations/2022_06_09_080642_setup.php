06f1ab1484dc1a77cf96d5b7423f2398834fb877
Binkap <real.desert.tiger@gmail.com>
2022-06-26 15:04:22 +0100

    fixes

--- database/migrations/2022_06_09_080642_setup.php
+++ database/migrations/2022_06_09_080642_setup.php
@@ -20,7 +20,7 @@ return new class extends Migration
     {
         $array = [
             'Technology',
-            'Bussiness',
+            'Business',
             'Investment',
             'Gadgets and Accessories',
             'Cars',
